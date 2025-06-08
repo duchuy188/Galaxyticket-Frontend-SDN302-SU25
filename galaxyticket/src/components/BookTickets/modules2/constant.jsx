2e87d80c7a4d22e5d983d0262a3f29e8eaeb6009
duchuy188 <huyndse173390@fpt.edu.vn>
2025-06-08 11:26:34 +0700

    delete

--- galaxyticket/src/components/BookTickets/modules2/constant.jsx
+++
@@ -1,3 +0,0 @@
-export const BOOK_TICKET_REQUEST = "detailRoomTicket/BOOK_TICKET_REQUEST";
-export const BOOK_TICKET_SUCCESS = "detailRoomTicket/BOOK_TICKET_SUCCESS";
-export const BOOK_TICKET_FAILED = "detailRoomTicket/BOOK_TICKET_FAILED";
