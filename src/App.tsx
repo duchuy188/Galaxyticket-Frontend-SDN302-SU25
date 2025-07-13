6aff1f38320cdd8d00b0b9415aaebe9d0ea8beea
lmhy220103 <leminhhy2212003@gmail.com>
2025-07-13 16:48:54 +0700

    cookies

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,28 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { AppRoutes } from './routes/routes';
+import useUserStatusCheck from './hooks/useUserStatusCheck';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export function App() {
+  useUserStatusCheck(); // Thêm dòng này để kiểm tra trạng thái user
+  
   return (
     <AuthProvider>
       <Router>
         <AppRoutes />
+        <ToastContainer
+          position="top-right"
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
       </Router>
     </AuthProvider>
   );
